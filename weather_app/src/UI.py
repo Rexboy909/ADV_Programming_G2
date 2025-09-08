a6759482c13ec5ffa8f0710db1b2f2034e59d98a
Rex <ulissespineda84@gmail.com>
2025-09-08 12:20:25 -0600

    Add basic GUI

--- weather_app/src/UI.py
+++ weather_app/src/UI.py
@@ -1,12 +1,95 @@
-from PySide6.QtWidgets import QApplication, QWidget, QMainWindow
+from PySide6.QtWidgets import QApplication, QWidget, QMainWindow, QVBoxLayout, QHBoxLayout, QLabel, QLineEdit, QDateTimeEdit, QTabWidget
 from PySide6.QtCore import QSize, Qt
 import sys
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-
         self.setWindowTitle("Weather App")
+
+        # Date and Location
+        dateLocation = QWidget() #date and location widget
+        dateLocationLayout = QHBoxLayout() #setting a horizontal layout
+        dateLocation.setLayout(dateLocationLayout) #setting the layout to the date and location widget
+
+
+        locationLabel = QLabel("Location")
+        locationLabel.setAlignment(Qt.AlignLeft | Qt.AlignTop) # aligning the label to the left
+
+        dateLocationLayout.addWidget(locationLabel) #adding the label to the layout
+
+        location = QLineEdit()
+        location.setPlaceholderText("Enter Location") 
+
+        dateLocationLayout.addWidget(location) #adding the input field to the layout
+
+        dateLabel = QLabel("Date") 
+        dateLabel.setAlignment(Qt.AlignRight | Qt.AlignTop)
+
+        dateLocationLayout.addWidget(dateLabel) #adding the date label to the layout
+
+        date = QDateTimeEdit() 
+        date.setCalendarPopup(True)
+        date.setDateTime(date.dateTime().currentDateTime()) #setting the current date and time as default
+        dateLocationLayout.addWidget(date) #adding the date input field to the layout
+        
+        # day tab content
+        todayTab = QWidget()
+        todayTabLayout = QHBoxLayout()
+        todayTab.setLayout(todayTabLayout)
+        todayStatsLayout = QVBoxLayout()
+        todayTabLayout.addLayout(todayStatsLayout)
+
+        todayTemp = QLabel("Temperature: ")
+        todayWindSpeed = QLabel("Wind Speed: ")
+        todayWindDirection = QLabel("Wind Direction: ")
+        todayHumidity = QLabel("Humidity: ")
+        todayPrecipitation = QLabel("Precipitation: ")
+        todayStatsLayout.addWidget(todayTemp)
+        todayStatsLayout.addWidget(todayWindSpeed)
+        todayStatsLayout.addWidget(todayWindDirection)
+        todayStatsLayout.addWidget(todayHumidity)
+        todayStatsLayout.addWidget(todayPrecipitation)
+
+        # Image support is possible
+        # todayimg = QPixmap()
+        # todayimg.load("weather_app/assets/sunny.png")
+        # todayimg = todayimg.scaled(QSize(100,100), Qt.KeepAspectRatio) #scaling the image to fit the label
+        # todayTabLayout.addWidget(QLabel(pixmap=todayimg)) #adding the image to the layout
+
+        # night tab content
+        tonightTab = QWidget()
+        tonightTabLayout = QHBoxLayout()
+        tonightTab.setLayout(tonightTabLayout)
+        tonightStatsLayout = QVBoxLayout()
+        tonightTabLayout.addLayout(tonightStatsLayout)
+
+        tonightTemp = QLabel("Temperature: ")
+        tonightWindSpeed = QLabel("Wind Speed: ")
+        tonightWindDirection = QLabel("Wind Direction: ")
+        tonightHumidity = QLabel("Humidity: ")
+        tonightPrecipitation = QLabel("Precipitation: ")
+        tonightStatsLayout.addWidget(tonightTemp)
+        tonightStatsLayout.addWidget(tonightWindSpeed)
+        tonightStatsLayout.addWidget(tonightWindDirection)
+        tonightStatsLayout.addWidget(tonightHumidity)
+        tonightStatsLayout.addWidget(tonightPrecipitation)
+
+        # Tabs setting
+        tabs = QTabWidget()
+        tabs.setTabPosition(QTabWidget.North)
+        tabs.addTab(todayTab, "Today")
+        tabs.addTab(tonightTab, "Tonight")
+
+        # Main Widget
+        main = QWidget()
+        mainLayout = QVBoxLayout() # creating a layout
+        main.setLayout(mainLayout) #setting the layout to the main widget. IDK why it has to be done this way but
+        mainLayout.addWidget(dateLocation)
+        mainLayout.addWidget(tabs)
+
+        self.setCentralWidget(main)
+
 wgui = QApplication(sys.argv)
 window = MainWindow()
 window.show()
