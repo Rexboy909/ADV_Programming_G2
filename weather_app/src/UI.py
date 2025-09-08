65c1e3c9bdc6a2b5fe2b11b1a786489dd6a63772
Rex <ulissespineda84@gmail.com>
2025-09-08 11:58:51 -0600

    fixed a comma

--- weather_app/src/UI.py
+++ weather_app/src/UI.py
@@ -1,4 +1,4 @@
-from PySide6.QtWidgets import QApplication, QWidget, QMainWindow,
+from PySide6.QtWidgets import QApplication, QWidget, QMainWindow
 from PySide6.QtCore import QSize, Qt
 import sys
 
